c4858deaf62bdd4504710f1413709439ef016882
Arash Kaffashi <arash_kaffashi@hotmail.com>
2022-10-25 13:51:37 -0400

    Solved lab

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,40 @@
 class Chronometer {
-  constructor() {
-    // ... your code goes here
-  }
+	constructor() {
+		this.currentTime = 0;
+		this.intervalId = null;
+	}
 
-  start(printTimeCallback) {
-    // ... your code goes here
-  }
+	start(printTimeCallback) {
+		this.intervalId = setInterval(() => {
+			this.currentTime++;
+			if (printTimeCallback) printTimeCallback();
+		}, 1000);
+	}
 
-  getMinutes() {
-    // ... your code goes here
-  }
+	getMinutes() {
+		return Math.floor(this.currentTime / 60);
+	}
 
-  getSeconds() {
-    // ... your code goes here
-  }
+	getSeconds() {
+		return this.currentTime % 60;
+	}
 
-  computeTwoDigitNumber(value) {
-    // ... your code goes here
-  }
+	computeTwoDigitNumber(value) {
+		return value.toString().padStart(2, "0");
+	}
 
-  stop() {
-    // ... your code goes here
-  }
+	stop() {
+		clearInterval(this.intervalId);
+		this.intervalId = null;
+	}
 
-  reset() {
-    // ... your code goes here
-  }
+	reset() {
+		this.currentTime = 0;
+	}
 
-  split() {
-    // ... your code goes here
-  }
+	split() {
+		let minutes = this.getMinutes();
+		let seconds = this.getSeconds();
+		return `${this.computeTwoDigitNumber(minutes)}:${this.computeTwoDigitNumber(seconds)}`;
+	}
 }
